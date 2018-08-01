a502e80be6f690b57372f4b012c4f168eeac0849
Zalron <33308793+Zalron@users.noreply.github.com>
2018-08-01 19:05:17 +1000

    Improved Movement Controllers and testing for Character Controllers Conponent
    Added bark and dig functions and mapped to R and RMB

--- NeighbourWoods/Assets/Project Scripts/Managers/PlayerManager.cs
+++ NeighbourWoods/Assets/Project Scripts/Managers/PlayerManager.cs
@@ -17,16 +17,27 @@ namespace Manager.Player
     {
         #region Variables
         public Vision vision;
+        #region Object Variables
         public GameObject player;
         public GameObject playerCamera;
+        public Transform playerCameraTransform;
         public PostProcessingBehaviour smellOVision;
         public GameObject[] smellObjects = new GameObject[0];
+        #endregion
         #region Controls Variables
         public float mouseSensitivity = 10;
-        public int forwardSpeed = 10;
-        public int backwardSpeed = 10;
-        public int leftSpeed = 10;
-        public int rightSpeed = 10;
+        public float walkSpeed = 2;
+        public float runSpeed = 6;
+        public float turnSmoothTime = 0.2f;
+        public float speedSmoothTime = 0.1f;
+        float turnSmoothVelocity;
+        float speedSmoothVelocity;
+        float currentSpeed;
+        //public int forwardSpeed = 10;
+        //public int backwardSpeed = 10;
+        //public int leftSpeed = 10;
+        //public int rightSpeed = 10;
+        public bool lockCursor;
         Vector3 currentRotation;
         Vector3 rotationSmoothVelocity;
         public Vector2 pitchMinMax = new Vector2(-40,85);
@@ -36,14 +47,20 @@ namespace Manager.Player
         float pitch = 0;
         #endregion
         #endregion
-        #region Start and Update Methods
+        #region Start() and Update()
         void Start() // Use this for initialization
         {
             player = GameObject.FindWithTag("Player");
             playerCamera = GameObject.FindWithTag("MainCamera");
+            playerCameraTransform = Camera.main.transform;
             smellOVision = GetComponentInChildren<PostProcessingBehaviour>();
             smellOVision.profile.vignette.enabled = false;
             vision = Vision.Normal;
+            if (lockCursor)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
         }
         void Update() // Update is called once per frame
         {
@@ -53,6 +70,8 @@ namespace Manager.Player
                     MovementController();
                     VisionController();
                     CameraController();
+                    BarkController();
+                    DigController();
                     break;
                 case GameState.Dialogue:
                     DialogueController();
@@ -68,26 +87,44 @@ namespace Manager.Player
         }
         #endregion
         #region Control Methods
+        #region MovementController()
         void MovementController() // The Function that moves the Player 
         {
-            if (Input.GetKey(KeyCode.W))
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Vector2 inputDir = input.normalized;
+            if (inputDir != Vector2.zero)
             {
-                transform.position += Vector3.forward * Time.deltaTime * forwardSpeed;
+                float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + playerCameraTransform.eulerAngles.y;
+                transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
             }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.position += Vector3.left * Time.deltaTime * leftSpeed;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.position += Vector3.right * Time.deltaTime * rightSpeed;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.position += Vector3.back * Time.deltaTime * backwardSpeed;
-            }
+            bool running = Input.GetKey(KeyCode.LeftShift); // Sprinting in Game
+            float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+            transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
+            // Animator?
+            //float animationSpeedPercent = ((running) ? 1:0.5f) * inputDir.magnitude;
+            //animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+            // Old movement code
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    transform.position += Vector3.forward * Time.deltaTime * forwardSpeed;
+            //}
+            //if (Input.GetKey(KeyCode.A))
+            //{
+            //    transform.position += Vector3.left * Time.deltaTime * leftSpeed;
+            //}
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    transform.position += Vector3.right * Time.deltaTime * rightSpeed;
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    transform.position += Vector3.back * Time.deltaTime * backwardSpeed;
+            //}
         }
-        void VisionController() // The Function that switches the 
+        #endregion
+        #region VisionController()
+        void VisionController() // The Function that switches the Vision enum event to smell or normal so that the colour blind camera comes on and the smell objects are turn on or off
         {
             if (Input.GetKeyDown(KeyCode.V))
             {
@@ -102,7 +139,9 @@ namespace Manager.Player
                 GameEvents.ReportVisionChange(vision);
             }
         }
-        void CameraController()
+        #endregion
+        #region CameraController()
+        void CameraController() // Controls the third person camera
         {
             yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
             pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
@@ -111,6 +150,25 @@ namespace Manager.Player
             float rotationSmoothTime = 0.12f;
             currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
             playerCamera.transform.eulerAngles = targetRotation;
+        }
+        #endregion
+        void JumpController()
+        {
+
+        }
+        void DigController()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Test Digging");
+            }
+        }
+        void BarkController()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("Bark");
+            }
         }
         void DialogueController()
         {
