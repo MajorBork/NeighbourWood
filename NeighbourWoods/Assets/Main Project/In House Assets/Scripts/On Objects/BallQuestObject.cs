5ef1f12397274ae1e9809cb8eacc5c397f7d0d76
Zalron <33308793+Zalron@users.noreply.github.com>
2018-09-02 17:51:44 +1000

    Building more of th block in scene.

---
+++ NeighbourWoods/Assets/Main Project/In House Assets/Scripts/On Objects/BallQuestObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using PixelCrushers.DialogueSystem;
+using Manager.Inventory;
+using Manager.UI;
+
+public class BallQuestObject : MonoBehaviour
+{
+    //private string inventoryManagerMethodPickupItem = "PickupItem";
+    private string questTitle = "BallQuest";
+    private string inventoryManagerMethodUpdateItemIcon = "UpdateItemIcon";
+    private QuestState newQuestState = QuestState.Success;
+    public GameObject ballObject;
+    public string message;
+    public Image ballIcon;
+    public InventoryManager inventoryManager;
+    public UIManager uiManager;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //if (Input.GetButtonDown("Dig"))
+            //{
+            //uiManager.SendMessage();
+            QuestLog.SetQuestState(questTitle, newQuestState);
+            DialogueManager.ShowAlert(message);
+            //inventoryManager.SendMessage(inventoryManagerMethodPickupItem, bananaIcon);
+            uiManager.SendMessage(inventoryManagerMethodUpdateItemIcon, ballIcon);
+            ballObject.SetActive(false);
+            //uiManager.
+            //}
+        }
+    }
+}
